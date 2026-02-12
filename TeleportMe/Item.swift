03379737de0f30894aff67ba30e730b9cc1d5dac
Daniel Burke <d2burke@gmail.com>
2026-02-11 21:13:18 -0500

    Initial Commit

---
+++ TeleportMe/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TeleportMe
+//
+//  Created by Daniel Burke on 2/11/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TeleportMeTests/TeleportMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeleportMeTests.swift
+//  TeleportMeTests
+//
+//  Created by Daniel Burke on 2/11/26.
+//
+
+import Testing
+@testable import TeleportMe
+
+struct TeleportMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
